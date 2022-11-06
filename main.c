58ea17d7d905bcebf412f5e1cfbf1f797bc5acc9
huangcpp <hjp1279@outlook.com>
2022-11-06 23:12:38 +0800

    线筛初试

--- main.c
+++ main.c
@@ -2,16 +2,18 @@
 #define 个数 100
 
 int main() {
-    int a[个数];
+    int a[个数]={0};
     for (int i = 1; i < 个数; ++i) {
         i=i+1;
-        for (int j = 2; j <= i; ++j) {
-            if(a[i]%j==0)
-            {
-                a[i]=0;
-                break;
-            }
-        }
+    }
+    for (int i = 1; i < 个数; ++i) {
+       if(a[i]!=0)
+       {
+           for (int j = i+1; j < 个数; j+=a[i]) {
+               a[j]=0;
+           }
+           a[i]=0;
+       }
     }
     for (int i = 0; i < 个数; ++i) {
         if(a[i]!=0)
