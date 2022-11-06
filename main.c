d6b569d5c23cc62fec056866c381df6704403471
huangcpp <hjp1279@outlook.com>
2022-11-06 23:29:33 +0800

    线筛初试

--- main.c
+++ main.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
-#define 个数 100
+#define 个数 1000
 
 int main() {
     int a[个数]={0};
     for (int i = 1; i < 个数; ++i) {
-        i=i+1;
+        a[i]=i+1;
     }
     for (int i = 1; i < 个数; ++i) {
        if(a[i]!=0)
        {
-           for (int j = i+1; j < 个数; j+=a[i]) {
+           for (int j = i + a[i]; j < 个数; j+=a[i]) {
                a[j]=0;
            }
-           a[i]=0;
+
        }
     }
     for (int i = 0; i < 个数; ++i) {
